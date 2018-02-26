7667a6e9d823e7cf3aaaf63520c78d4f01dcea71
Lauren Choi <laurenchoi@gmail.com>
2018-02-26 00:04:14 -0600

    Delete ScrabbleProgramClient.java

--- ScrabbleProgramClient.java
+++
@@ -1,8 +0,0 @@
-public class ScrabbleProgramClient {
-
-	public static void main(String[] args) {
-		ScrabbleProgram scrabble1 = new ScrabbleProgram();
-		System.out.println(scrabble1.computeScore("hippopotamus"));
-	}
-
-}
